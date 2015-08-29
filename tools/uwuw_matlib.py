f731caa1cb6a427c31ba25b03bfd5d3e6ce017aa
Elliott Biondo <elliott.biondo@gmail.com>
2015-08-28 23:08:48 -0500

    script is working, but need to figure out naming corner cases

--- tools/uwuw_matlib.py
+++ tools/uwuw_matlib.py
@@ -8,14 +8,22 @@ from pyne.mcnp import mats_from_inp
 from pyne.material import Material, MultiMaterial, MaterialLibrary
 
 def uwuw_matlib(inp, out):
-    mats = mats_from_(inp)
+    mats = mats_from_inp(inp)
+    print mats
     uwuw_mats = {}
-    for mat in mats:
-        if istype(mat, Material):
-            mat_name = "mat:mat_"
-
+    for mat in mats.values():
+        if isinstance(mat, Material):
+            mat_name = "mat:m{0}/rho:{1}".format(mat.metadata["mat_number"], mat.density)
+            mat.metadata["name"] = mat_name
+            uwuw_mats[mat_name] = mat
+        if isinstance(mat, MultiMaterial):
+           for m in mat._mats.keys():
+               mat_name = "mat:m{0}/rho:{1}".format(m.metadata["mat_number"], m.density)
+               m.metadata["name"] = mat_name
+               uwuw_mats[mat_name] = m
+           
     ml = MaterialLibrary(uwuw_mats)
-    ml.write_hdf5("out")
+    ml.write_hdf5(out)
 
 def main():
     parser = argparse.ArgumentParser(description="This program reads")
