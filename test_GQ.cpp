88762a4acb369aac82505b92252bbd0a06a90fde
Patrick Shriwise <pshriwise@gmail.com>
2019-08-28 14:33:37 -0500

    Some test formatting updates.

--- test_GQ.cpp
+++ test_GQ.cpp
@@ -14,79 +14,80 @@ int check_gq_type(int test_num, GQ_Characterize* gq, GQ_TYPE expected_type, std:
   }
 }
 
-int main(){
+int main() {
 
   int failed_tests = 0;
   std::stringstream err_msg;
 
-// All unit tests currently successful when special case line is fully decommented
-  //Should print ELLIPSOID
+  // All unit tests currently successful when special case line is fully decommented
+  // ELLIPSOID
   GQ_Characterize* testEllip = new GQ_Characterize(1.0,1.0,1.0, 0.0,0.0,0.0,0.0,0.0,0.0,-1.0);
   failed_tests += check_gq_type(1, testEllip, GQ_TYPE::ELLIPSOID, err_msg);
   delete testEllip;
 
-  //ONE_SHEET_HYPERBOLOID
+  // ONE_SHEET_HYPERBOLOID
   GQ_Characterize* testOneSheet = new GQ_Characterize(1.0,1.0,-1.0, 0.0,0.0,0.0,0.0,0.0,0.0,-1.0);
   failed_tests += check_gq_type(2, testOneSheet, GQ_TYPE::ONE_SHEET_HYPERBOLOID, err_msg);
   delete testOneSheet;
 
-  //TWO_SHEET_HYPERBOLOID
+  // TWO_SHEET_HYPERBOLOID
   GQ_Characterize* testTwoSheet = new GQ_Characterize(-1.0,-1.0,1.0, 0.0,0.0,0.0,0.0,0.0,0.0,-1.0);
   failed_tests += check_gq_type(3, testTwoSheet, GQ_TYPE::TWO_SHEET_HYPERBOLOID, err_msg);
   delete testTwoSheet;
 
-  //ELLIPTIC_CONE
+  // ELLIPTIC_CONE
   GQ_Characterize* testEllCone = new GQ_Characterize(1.0,1.0,-1.0, 0.0,0.0,0.0,0.0,0.0,0.0,0.0);
   failed_tests += check_gq_type(4, testEllCone, GQ_TYPE::ELLIPTIC_CONE, err_msg);
   delete testEllCone;
 
-  //ELLIPTIC_PARABOLOID
+  // ELLIPTIC_PARABOLOID
   GQ_Characterize* testEllPara = new GQ_Characterize(1.0,1.0, 0.0,0.0,0.0,0.0,0.0,0.0,-1.0,0.0);
   failed_tests += check_gq_type(5, testEllPara, GQ_TYPE::ELLIPTIC_PARABOLOID, err_msg);
   delete testEllPara;
 
-  //HYPERBOLIC_PARABOLOID
+  // HYPERBOLIC_PARABOLOID
   GQ_Characterize* testHypPara = new GQ_Characterize(1.0,-1.0, 0.0,0.0,0.0,0.0,0.0,0.0,-1.0,0.0);
   failed_tests += check_gq_type(6, testHypPara, GQ_TYPE::HYPERBOLIC_PARABOLOID, err_msg);
   delete testHypPara;
 
-  //ELLIPTIC_CYL
+  // ELLIPTIC_CYL
   GQ_Characterize* testEllCyl = new GQ_Characterize(1.0,1.0, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,-1.0);
   failed_tests += check_gq_type(7, testEllCyl, GQ_TYPE::ELLIPTIC_CYL, err_msg);
   delete testEllCyl;
 
-  //HYPERBOLIC_CYL
+  // HYPERBOLIC_CYL
   GQ_Characterize* testHypCyl = new GQ_Characterize(1.0,-1.0, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0);
   failed_tests += check_gq_type(8, testHypCyl, GQ_TYPE::HYPERBOLIC_CYL, err_msg);
   delete testHypCyl;
 
-  //PARABOLIC_CYL
+  // PARABOLIC_CYL
   GQ_Characterize* testParaCyl = new GQ_Characterize(1.0, 0.0,0.0,0.0,0.0,0.0,0.0,-1.0,0.0,0.0);
   failed_tests += check_gq_type(9, testParaCyl, GQ_TYPE::PARABOLIC_CYL, err_msg);
   delete testParaCyl;
 
-//Begin Rotation tests
-  //Should return ELLIPSOID
+  // Transformation Tests
+
+  // ELLIPSOID
   GQ_Characterize* testRotEllip = new GQ_Characterize(103,125,66,-48,-12,-60,0,0,0,-294);
   failed_tests += check_gq_type(10, testRotEllip, GQ_TYPE::ELLIPSOID, err_msg);
   delete testRotEllip;
 
-  //ELLIPTIC_CONE
+  // ELLIPTIC_CONE
   GQ_Characterize* testRotCone = new GQ_Characterize(3,3,-1,2, 0,0,0,0,0,0);
   failed_tests += check_gq_type(11, testRotCone, GQ_TYPE::ELLIPTIC_CONE, err_msg);
   delete testRotCone;
 
-  //ELLIPTIC_PARABOLOID
+  // ELLIPTIC_PARABOLOID
   GQ_Characterize* testRotEllParab = new GQ_Characterize(1,3,1,2,2,2,-2,4,2,12);
   failed_tests += check_gq_type(12, testRotEllParab, GQ_TYPE::ELLIPTIC_PARABOLOID, err_msg);
   delete testRotEllParab;
 
-  //ELLIPTIC_CYLINDER
+  // ELLIPTIC_CYLINDER
   GQ_Characterize* testRotEllCyl = new GQ_Characterize(5,2,5,-4,-4,-2,6,-12,18,-3);
   failed_tests += check_gq_type(13, testRotEllCyl, GQ_TYPE::ELLIPTIC_CYL, err_msg);
   delete testRotEllCyl;
 
-  //PARABOLIC CYLINDER
+  // PARABOLIC CYLINDER
   GQ_Characterize* testRotParaCyl = new GQ_Characterize(9, 36, 4, -36, -24, 12, -16, -24, -48, 56);
   failed_tests += check_gq_type(14, testRotParaCyl, GQ_TYPE::PARABOLIC_CYL, err_msg);
   delete testRotParaCyl;
